1abe9ee4e75a00845adf61defc136e432cd109c0
khairul islam <khairulsagor567@gmail.com>
2023-06-10 01:56:55 +0600

    28. Find the Index of the First Occurrence in a String

---
+++ js/28. Find the Index of the First Occurrence in a String.js
@@ -0,0 +1,13 @@
+const word1 = "sadbutsad"; const word2 = "sad";
+
+var strStr = function(haystack, needle) 
+{
+    return haystack.indexOf(needle);
+};
+
+console.log(strStr(word1,word2));
+
+
+
+
+
