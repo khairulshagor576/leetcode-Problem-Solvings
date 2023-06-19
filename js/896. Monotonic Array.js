6a4b8bf917ddea3241badaa4ab7deb9c6bd3a2b2
khairulshagor576 <khairulshagor@outlook.com>
2023-06-19 09:35:22 +0600

    896. Monotonic Array

---
+++ js/896. Monotonic Array.js
@@ -0,0 +1,24 @@
+const nums = [1,2,2,3];
+
+var isMonotonic = function(digits) {
+
+  let increasing = true;
+    let decreasing = true;
+
+    for (let i = 1; i< nums.length; i++) {
+        let prev = nums[i-1]
+        let curr = nums[i]
+        if (curr < prev) {
+            increasing = false;
+        }
+        if (curr > prev) {
+            decreasing = false
+        }
+    }
+
+    return increasing || decreasing
+
+
+};
+
+console.log(isMonotonic(nums));
