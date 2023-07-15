589f5b3c27a85ff115006aa6db3af0126787ac3d
Mohammad Khairul Islam <76487763+khairulshagor576@users.noreply.github.com>
2023-07-15 12:52:02 +0600

    Update 1. Two Sum.js

--- js/1. Two Sum.js
+++ js/1. Two Sum.js
@@ -4,7 +4,7 @@ const nums = [2,7,11,15,22], target = 22;
 var twoSum = function(nums, target) 
 {
     for (let i = 0; i < nums.length; i++) {
-        for (let j=1; j < nums.length; j++) {
+        for (let j=i+1; j < nums.length; j++) {
             if(nums[i]+nums[j]===target)
             {
                 return [i,j];
