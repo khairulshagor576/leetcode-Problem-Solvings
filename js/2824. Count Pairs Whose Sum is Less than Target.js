dfd81bb4ff3ecfbb81b3ebbe28aa0fa478aedb8c
khairulshagor576 <khairulshagor@outlook.com>
2023-09-02 16:20:20 +0600

    2824. Count Pairs Whose Sum is Less than Target.

---
+++ js/2824. Count Pairs Whose Sum is Less than Target.js
@@ -0,0 +1,20 @@
+
+const nums = [-1,1,2,3,1], target = 2;
+
+var countPairs = function(nums, target) 
+{
+   let count=0;
+   
+   for(let i = 0;i<nums.length;i++)
+   {
+     for(let j=i+1;j<nums.length;j++)
+     {
+         if(i<j && nums[i]+nums[j]<target){
+           count++;
+         }   
+     }
+   }
+   return count;
+};
+
+console.log(countPairs(nums,target));
