ae0b1c43f236f270fbd3c8deb55958fb7f572d3e
khairulshagor576 <khairulshagor@outlook.com>
2023-09-12 18:10:26 +0600

    2000. Reverse Prefix of Word

---
+++ js/2000. Reverse Prefix of Word.js
@@ -0,0 +1,20 @@
+const word = "abcdefd", ch = "d";
+
+var reversePrefix = function(word, ch) {
+  
+    let left = 0;
+    let right = word.indexOf(ch);
+    
+    if(right === -1) return word;
+       
+    word = word.split('');
+    while(left <= right) {
+        [word[left], word[right]] = [word[right], word[left]];    
+        left++;
+        right--;
+    }
+    
+    return word.join('');
+};
+
+console.log(reversePrefix(word, ch));
